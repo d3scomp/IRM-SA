4d4d4c834aa8e69ddaaa9f1bd0ac62b3796face8
Michal Kit <kit@d3s.mff.cuni.cz>
2014-10-04 10:50:22 +0200

    Fixing the demo. Removing the MATSim integration as it is not needed in this case. To make it work the Maven repository containgin the jdeeco-core.jar and jdeeco-simulation.jar needs to be updated.

--- cz.cuni.mff.d3s.jdeeco.irm-sa.demo/src/firefighters/DecentralizedRun.java
+++ cz.cuni.mff.d3s.jdeeco.irm-sa.demo/src/firefighters/DecentralizedRun.java
@@ -15,13 +15,10 @@
  ******************************************************************************/
 package firefighters;
 
-import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 
-import org.matsim.api.core.v01.Id;
-import org.matsim.core.basic.v01.IdImpl;
-
-import tutorial.environment.MATSimDataProviderReceiver;
-import tutorial.matsim.PopulationAgentSource;
 import cz.cuni.mff.d3s.deeco.annotations.processor.AnnotationProcessor;
 import cz.cuni.mff.d3s.deeco.annotations.processor.AnnotationProcessorException;
 import cz.cuni.mff.d3s.deeco.annotations.processor.AnnotationProcessorExtensionPoint;
@@ -31,11 +28,11 @@ import cz.cuni.mff.d3s.deeco.model.runtime.api.ComponentInstance;
 import cz.cuni.mff.d3s.deeco.model.runtime.api.RuntimeMetadata;
 import cz.cuni.mff.d3s.deeco.model.runtime.custom.RuntimeMetadataFactoryExt;
 import cz.cuni.mff.d3s.deeco.runtime.RuntimeFramework;
+import cz.cuni.mff.d3s.deeco.simulation.DelayedKnowledgeDataHandler;
 import cz.cuni.mff.d3s.deeco.simulation.DirectSimulationHost;
+import cz.cuni.mff.d3s.deeco.simulation.JDEECoSimulation;
 import cz.cuni.mff.d3s.deeco.simulation.SimulationRuntimeBuilder;
-import cz.cuni.mff.d3s.deeco.simulation.matsim.JDEECoAgent;
-import cz.cuni.mff.d3s.deeco.simulation.matsim.JDEECoAgentSource;
-import cz.cuni.mff.d3s.deeco.simulation.matsim.MATSimSimulation;
+import cz.cuni.mff.d3s.deeco.simulation.TimerTaskListener;
 import cz.cuni.mff.d3s.irm.model.design.IRM;
 import cz.cuni.mff.d3s.irm.model.design.IRMDesignPackage;
 import cz.cuni.mff.d3s.irm.model.trace.api.TraceModel;
@@ -44,18 +41,14 @@ import cz.cuni.mff.d3s.irmsa.EMFHelper;
 
 public class DecentralizedRun {
 
-	static final String MODELS_BASE_PATH = "designModels/";
-	static final String XMIFILE_PREFIX = "firefighters_";
-	static final String DESIGN_MODEL_PATH = MODELS_BASE_PATH + "firefighters.irmdesign";
-	
-	private static final String MATSIM_CONFIG_CUSTOM = "input/config.xml";
+	private static final String MODELS_BASE_PATH = "designModels/";
+	private static final String DESIGN_MODEL_PATH = MODELS_BASE_PATH + "firefighters.irmdesign";
+	private static final long SIMULATION_START = 0; // in milliseconds
+	private static final long SIMULATION_END = 10000; // in milliseconds
+	private static final long NETWORK_DELAY = 100; // in milliseconds
 	
 	private static IRM design;
-
-	private static JDEECoAgentSource jdeecoAgentSource;
-	private static MATSimSimulation simulation;
-	private static MATSimDataProviderReceiver matSimProviderReceiver;
-
+	private static JDEECoSimulation simulation;
 	private static SimulationRuntimeBuilder builder;
 
 	public static void main(String args[]) throws AnnotationProcessorException, InterruptedException {
@@ -64,61 +57,36 @@ public class DecentralizedRun {
 		@SuppressWarnings("unused")
 		IRMDesignPackage p = IRMDesignPackage.eINSTANCE; 
 		design = (IRM) EMFHelper.loadModelFromXMI(DESIGN_MODEL_PATH);
-		
-		jdeecoAgentSource = new JDEECoAgentSource();
-		PopulationAgentSource populationAgentSource = new PopulationAgentSource();
-		
-		matSimProviderReceiver = new MATSimDataProviderReceiver();
-		simulation = new MATSimSimulation(matSimProviderReceiver, matSimProviderReceiver, null, null, Arrays.asList(jdeecoAgentSource, populationAgentSource), MATSIM_CONFIG_CUSTOM);
+
+		DelayedKnowledgeDataHandler networkKnowledgeDataHandler = new DelayedKnowledgeDataHandler(NETWORK_DELAY);
+		simulation = new JDEECoSimulation(SIMULATION_START, SIMULATION_END, networkKnowledgeDataHandler);
 		
 		builder = new SimulationRuntimeBuilder();
+		
+		List<TimerTaskListener> listeners = new LinkedList<>();
+		listeners.add(networkKnowledgeDataHandler);
 
-		createAndDeployGroupMember(42,"1_1");
-		createAndDeployGroupMember(43,"11_11");
-		createAndDeployGroupLeader(1,"5_5");
+		createAndDeployGroupMember(42,"1_1", listeners);
+		createAndDeployGroupMember(43,"11_11", listeners);
+		createAndDeployGroupLeader(1,"5_5", listeners);
 		
 		simulation.run();
 		Log.i("Simulation Finished");
 	}
 	
-	private static void createAndDeployGroupMember(int idx, String sourceLinkIdString) throws AnnotationProcessorException {
+	private static void createAndDeployGroupMember(int idx, String sourceLinkIdString, Collection<? extends TimerTaskListener> simulationListeners) throws AnnotationProcessorException {
 		String compIdString = "M" + idx;
-		Id compId = new IdImpl(compIdString);
-		Id sourceLinkId = new IdImpl(sourceLinkIdString);
-
-		jdeecoAgentSource.addAgent(new JDEECoAgent(compId, sourceLinkId));
-
 		GroupMember component = new GroupMember(compIdString,"L1");
-		
-		RuntimeMetadata model = RuntimeMetadataFactoryExt.eINSTANCE.createRuntimeMetadata();
-		TraceModel trace = TraceFactory.eINSTANCE.createTraceModel();
-		AnnotationProcessorExtensionPoint extension = new IrmAwareAnnotationProcessorExtension(design,trace);
-		AnnotationProcessor processor = new AnnotationProcessor(RuntimeMetadataFactoryExt.eINSTANCE, model, extension);
-		processor.process(component);//, new AdaptationManager());
-		
-		// pass design and trace models to the AdaptationManager
-		for (ComponentInstance c : model.getComponentInstances()) {
-			if (c.getName().equals(AdaptationManager.class.getName())) {
-				c.getInternalData().put(AdaptationManager.DESIGN_MODEL, design);
-				c.getInternalData().put(AdaptationManager.TRACE_MODEL, trace);
-			}
-		}
-		
-		DirectSimulationHost host = simulation.getHost(compIdString);
-		RuntimeFramework runtime = builder.build(host, simulation, model, null, null);
-		
-		runtime.start();		
+		createAndDeployComponent(component, compIdString, simulationListeners);
 	}
 	
-	private static void createAndDeployGroupLeader(int idx, String sourceLinkIdString) throws AnnotationProcessorException {
+	private static void createAndDeployGroupLeader(int idx, String sourceLinkIdString, Collection<? extends TimerTaskListener> simulationListeners) throws AnnotationProcessorException {
 		String compIdString = "L" + idx;
-		Id compId = new IdImpl(compIdString);
-		Id sourceLinkId = new IdImpl(sourceLinkIdString);
-
-		jdeecoAgentSource.addAgent(new JDEECoAgent(compId, sourceLinkId));
-
 		GroupLeader component = new GroupLeader(compIdString);
-		
+		createAndDeployComponent(component, compIdString, simulationListeners);
+	}
+	
+	private static void createAndDeployComponent(Object component, String hostId, Collection<? extends TimerTaskListener> simulationListeners) throws AnnotationProcessorException {
 		RuntimeMetadata model = RuntimeMetadataFactoryExt.eINSTANCE.createRuntimeMetadata();
 		TraceModel trace = TraceFactory.eINSTANCE.createTraceModel();
 		AnnotationProcessorExtensionPoint extension = new IrmAwareAnnotationProcessorExtension(design,trace);
@@ -133,9 +101,9 @@ public class DecentralizedRun {
 			}
 		}
 		
-		DirectSimulationHost host = simulation.getHost(compIdString);
-		RuntimeFramework runtime = builder.build(host, simulation, model, null, null);
+		DirectSimulationHost host = simulation.getHost(hostId);
+		RuntimeFramework runtime = builder.build(host, simulation, simulationListeners, model, null, null);
 		
-		runtime.start();		
+		runtime.start();	
 	}
 }
