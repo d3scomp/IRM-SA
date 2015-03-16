c2e5094de97a7c0874562d4f06c887132e135486
Ilias Gerostathopoulos <iliasger@gmail.com>
2015-03-16 17:13:30 +0100

    Decentralized deployment using the new DEECo network plugins

--- src/deeco/demo/CorrelationTest.java
+++ src/deeco/demo/CorrelationTest.java
@@ -6,31 +6,35 @@ import cz.cuni.mff.d3s.deeco.runtime.DEECoException;
 import cz.cuni.mff.d3s.deeco.runtime.DEECoNode;
 import cz.cuni.mff.d3s.deeco.timer.DiscreteEventTimer;
 import cz.cuni.mff.d3s.deeco.timer.SimulationTimer;
+import cz.cuni.mff.d3s.jdeeco.network.Network;
+import cz.cuni.mff.d3s.jdeeco.network.device.BroadcastLoopback;
+import cz.cuni.mff.d3s.jdeeco.network.l2.strategy.KnowledgeInsertingStrategy;
+import cz.cuni.mff.d3s.jdeeco.publishing.DummyKnowledgePublisher;
 /**
  * @author Ilias Gerostathopoulos <iliasg@d3s.mff.cuni.cz>
  */
 public class CorrelationTest {
 	
-	public static void main(String[] args) throws DEECoException, AnnotationProcessorException {
+	public static void main(String[] args) throws DEECoException, AnnotationProcessorException, InstantiationException, IllegalAccessException {
 
-		CorrelationPlugin correlationPlugin = new CorrelationPlugin();
-		
 		SimulationTimer simulationTimer = new DiscreteEventTimer();
 		DEECoSimulation realm = new DEECoSimulation(simulationTimer);
+		realm.addPlugin(new BroadcastLoopback());
+		realm.addPlugin(Network.class);
+		realm.addPlugin(DummyKnowledgePublisher.class);
+		realm.addPlugin(KnowledgeInsertingStrategy.class);
 		
-		DEECoNode deeco = realm.createNode(correlationPlugin);
-				
-		/* deploy components and ensembles */
-		deeco.deployComponent(new GroupMember("1"));
-		deeco.deployComponent(new GroupMember("2"));
-		deeco.deployComponent(new GroupLeader("3"));
+		DEECoNode deeco1 = realm.createNode();
+		deeco1.deployComponent(new GroupMember("1"));
 		
-		/* decentralized case */
-//		DEECoNode deeco = realm.createNode(); 
-//		deeco.deployComponent(new GroupMember("1"));
-//		deeco.deployComponent(new GroupMember("2"));
-//		deeco.deployComponent(new GroupLeader("3"));
-//		DEECoNode correlationNode = realm.createNode(correlationPlugin);
+		DEECoNode deeco2 = realm.createNode();
+		deeco2.deployComponent(new GroupMember("2"));
+		
+		DEECoNode deeco3 = realm.createNode();
+		deeco3.deployComponent(new GroupLeader("3"));
+		
+		/* Create node that holds the correlation component */
+		realm.createNode(new CorrelationPlugin());
 		
 		/* WHEN simulation is performed */
 		realm.start(1000);
