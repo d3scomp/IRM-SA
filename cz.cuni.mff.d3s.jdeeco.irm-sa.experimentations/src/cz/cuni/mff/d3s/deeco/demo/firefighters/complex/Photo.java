a9e4d59dd748f782f4cb8fca065c71619c1168d0
Ilias Gerostathopoulos <iliasger@gmail.com>
2015-04-24 16:05:26 +0200

    removing unrelated projects from branch

--- cz.cuni.mff.d3s.jdeeco.irm-sa.experimentations/src/cz/cuni/mff/d3s/deeco/demo/firefighters/complex/Photo.java
+++
@@ -1,9 +0,0 @@
-package cz.cuni.mff.d3s.deeco.demo.firefighters.complex;
-
-import java.io.Serializable;
-
-public class Photo implements Serializable {
-
-	private static final long serialVersionUID = -5098728621651588706L;
-
-}
--- cz.cuni.mff.d3s.jdeeco.irm-sa.experimentations/src/cz/cuni/mff/d3s/deeco/demo/firefighters/complex/ThermalPhoto.java
+++
@@ -1,9 +0,0 @@
-package cz.cuni.mff.d3s.deeco.demo.firefighters.complex;
-
-import java.io.Serializable;
-
-public class ThermalPhoto implements Serializable {
-
-	private static final long serialVersionUID = 270816998183900640L;
-
-}
