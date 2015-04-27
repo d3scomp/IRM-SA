34fe25025cdb10256f9d7b8aa10ec9fb8b15a81d
Ilias Gerostathopoulos <iliasger@gmail.com>
2015-04-27 11:16:10 +0200

    removing unrelated projects

--- irm-sa.strategies/src/cz/cuni/mff/d3s/irmsa/strategies/commons/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Holds the classes responsible for evolution adaptation.
- */
-package cz.cuni.mff.d3s.irmsa.strategies.commons;
--- irm-sa.strategies/src/cz/cuni/mff/d3s/irmsa/strategies/commons/variations/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Holds the plugin interfaces for evolution adaptation.
- */
-package cz.cuni.mff.d3s.irmsa.strategies.commons.variations;
